2f9fd06477022fa02b4e9bd4648e4397e58939d0
akhil-007 <sharathananthula9@gmail.com>
2025-04-23 16:19:34 +0530

    Updated gunicorn configuration and requirements for Railway deployment

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 workers = 1
 timeout = 120
-worker_class = 'gevent'
+worker_class = 'sync'  # Changed from 'gevent' to 'sync'
 worker_connections = 1000
 preload_app = True  # This helps with memory usage during startup
